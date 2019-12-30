e0b50a3acca49d166dc2ce59fa6c0e962567f177
Elias Johansson <elias.elle.johansson@gmail.com>
2019-12-30 23:53:58 +0100

    Slight refraction of the code. Added explanatory text to the side of the chart to add further information.

--- read_excel.py
+++ read_excel.py
@@ -3,65 +3,40 @@ import xlrd
 import matplotlib.pyplot as plt
 
 
-def write_graph(components):
-    # Pie-chart of two separate charts.
-    fig1, ax1 = plt.subplots()
-
-    # Chart 1: Represent the different categories with how much money was spent on them.
-    labels = 'Food', 'Bills', 'Pleasure', 'Clothes', 'Others'
-    explode = (0, 0, 0, 0, 0.1)  # only "explode" the 2nd slice
-    ax1.pie(components, explode=explode, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.show()
-
-
 class FinancialGraphic:
+    path = ""
     expenses_list = []
     food_expenses = 0
     bills_expenses = 0
     pleasure_expenses = 0
     clothes_expenses = 0
     other_expenses = 0
-    path = ""
+    total_expenses = 0
 
     # Different categories
-    food = ['de fyra arstiderna', 'ica', 'eurest', 'vallastaden rest', '7 eleven', 'city gross']
+    food = ['fyra', 'de fyra arstiderna', 'ica', 'eurest', 'vallastaden rest', '7 eleven', 'city gross']
     bills = ['telia', 'bredband2', 'spotify', 'heimstaden']
     pleasure = ['blizzard']
     clothes = ['mq', 'dressman', 'gant']
 
     def __init__(self, path_to_file):
-        # Give the location of the file
-        self.other_list = []
-        self.path_to_file = path_to_file
-        expenses_list = []
-        self.food_expenses = 0
-        self.bills_expenses = 0
-        self.pleasure_expenses = 0
-        self.clothes_expenses = 0
-        self.other_expenses = 0
-
         # Add all the expenses to a list to make it easier to handle them.
-        expenses_list.append(self.food_expenses)
-        expenses_list.append(self.bills_expenses)
-        expenses_list.append(self.pleasure_expenses)
-        expenses_list.append(self.clothes_expenses)
-        expenses_list.append(self.other_expenses)
+        self.expenses_list.append(self.food_expenses)
+        self.expenses_list.append(self.bills_expenses)
+        self.expenses_list.append(self.pleasure_expenses)
+        self.expenses_list.append(self.clothes_expenses)
+        self.expenses_list.append(self.other_expenses)
 
         # To open Workbook
         wb = xlrd.open_workbook(path_to_file)
         sheet = wb.sheet_by_index(0)
+
+        # Populate the categories given the data in the excel file.
         self.populate_categories(sheet)
 
-        print("Food: {0}\nBills: {1}\nPleasure: {2}\nClothes: {3}\nOther: {4}".format(self.food_expenses,
-                                                                                      self.bills_expenses,
-                                                                                      self.pleasure_expenses,
-                                                                                      self.clothes_expenses,
-                                                                                      self.other_expenses))
-        write_graph(
-            [abs(self.food_expenses), abs(self.bills_expenses), abs(self.pleasure_expenses), abs(self.clothes_expenses),
-             abs(self.other_expenses)])
+        self.draw_graph(
+            [self.food_expenses, self.bills_expenses, self.pleasure_expenses, self.clothes_expenses,
+             self.other_expenses])
 
     def populate_categories(self, sheet):
         for row in range(sheet.nrows):
@@ -71,29 +46,63 @@ class FinancialGraphic:
                 amount = amount.replace('.', '')
                 amount = amount.replace(',', '.')
                 amount = float(amount)
+                amount = abs(amount)
+                amount = abs(amount)
 
                 for category in self.food:
                     if category in transaction_info:
                         self.food_expenses += amount
+                        self.total_expenses += amount
 
                 for category in self.bills:
                     if category in transaction_info:
                         self.bills_expenses += amount
+                        self.total_expenses += amount
 
                 for category in self.pleasure:
                     if category in transaction_info:
                         self.pleasure_expenses += amount
+                        self.total_expenses += amount
 
                 for category in self.clothes:
                     if category in transaction_info:
                         self.clothes_expenses += amount
+                        self.total_expenses += amount
 
-                else:
-                    self.other_expenses += amount
-                    self.other_list.append(transaction_info)
+                # Round the total amount to two decimals.
+                self.total_expenses = float("{0:.2f}".format(self.total_expenses))
 
             except ValueError as e:
                 print(e)
+
+    def draw_graph(self, components):
+        # Pie-chart of two separate charts.
+        fig1, plot = plt.subplots()
+
+        # Chart 1: Represent the different categories with how much money was spent on them.
+        labels = 'Food', 'Bills', 'Pleasure', 'Clothes', 'Others'
+        explode = (0.1, 0, 0, 0, 0)  # only "explode" the 2nd slice
+
+        # White ring for the middle of the doughnut
+        middle_circle = plt.Circle((0, 0), 0.7, color='white')
+
+        plot.pie(components, explode=explode, labels=labels, autopct='%1.1f%%',
+                 shadow=True, startangle=90, pctdistance=0.85, labeldistance=1.2)
+
+        p = plt.gcf()
+        p.gca().add_artist(middle_circle)
+
+        # List of elements to the side of the chart. (Legends)
+        # plot.legend(bbox_to_anchor=(1.1, 1.05), fancybox=True, shadow=True)
+
+        # Explanatory text to complement the text.
+        plot.text(1.5, 0, 'Food: {0} SEK'.format(self.food_expenses))
+        plot.text(1.5, 0.1, 'Bills: {0} SEK'.format(self.bills_expenses))
+        plot.text(1.5, 0.2, 'Pleasure: {0} SEK'.format(self.pleasure_expenses))
+        plot.text(1.5, 0.3, 'Other: {0} SEK'.format(self.other_expenses))
+        plot.text(1.5, -0.2, 'Total expenses: {0} SEK'.format(self.total_expenses))
+
+        plt.show()
 
 
 if __name__ == '__main__':
