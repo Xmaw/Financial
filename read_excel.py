69027506dba5f5feb4d02986f1423dbd777d9987
Elias Johansson <elias.elle.johansson@gmail.com>
2019-12-31 00:41:19 +0100

    Added functionality to track income and payback of loans.

--- read_excel.py
+++ read_excel.py
@@ -6,27 +6,36 @@ import matplotlib.pyplot as plt
 class FinancialGraphic:
     path = ""
     expenses_list = []
+    other_list = []
+
+    # Outgoing expenses
     food_expenses = 0
     bills_expenses = 0
     pleasure_expenses = 0
     clothes_expenses = 0
     other_expenses = 0
     total_expenses = 0
+    payback_loans_amount = 0
+
+    # Income
+    income_amount = 0
 
     # Different categories
-    food = ['fyra', 'de fyra arstiderna', 'ica', 'eurest', 'vallastaden rest', '7 eleven', 'city gross']
+    food = ['fyra arstider',
+            'de fyra arstiderna',
+            'ica', 'eurest',
+            'vallastadens rest',
+            '7 eleven',
+            'city gross',
+            'restaurang skyline',
+            'krubbstugan']
     bills = ['telia', 'bredband2', 'spotify', 'heimstaden']
-    pleasure = ['blizzard']
+    pleasure = ['blizzard', 'netflix', 'hbo', 'frisor']
     clothes = ['mq', 'dressman', 'gant']
+    income = ['lön', 'lån']
+    payback_loans = ['centrala studie']
 
     def __init__(self, path_to_file):
-        # Add all the expenses to a list to make it easier to handle them.
-        self.expenses_list.append(self.food_expenses)
-        self.expenses_list.append(self.bills_expenses)
-        self.expenses_list.append(self.pleasure_expenses)
-        self.expenses_list.append(self.clothes_expenses)
-        self.expenses_list.append(self.other_expenses)
-
         # To open Workbook
         wb = xlrd.open_workbook(path_to_file)
         sheet = wb.sheet_by_index(0)
@@ -36,7 +45,7 @@ class FinancialGraphic:
 
         self.draw_graph(
             [self.food_expenses, self.bills_expenses, self.pleasure_expenses, self.clothes_expenses,
-             self.other_expenses])
+             self.other_expenses, self.payback_loans_amount])
 
     def populate_categories(self, sheet):
         for row in range(sheet.nrows):
@@ -47,41 +56,68 @@ class FinancialGraphic:
                 amount = amount.replace(',', '.')
                 amount = float(amount)
                 amount = abs(amount)
-                amount = abs(amount)
 
+                category_found = False
                 for category in self.food:
                     if category in transaction_info:
                         self.food_expenses += amount
                         self.total_expenses += amount
+                        category_found = True
 
                 for category in self.bills:
                     if category in transaction_info:
                         self.bills_expenses += amount
                         self.total_expenses += amount
+                        category_found = True
 
                 for category in self.pleasure:
                     if category in transaction_info:
                         self.pleasure_expenses += amount
                         self.total_expenses += amount
+                        category_found = True
 
                 for category in self.clothes:
                     if category in transaction_info:
                         self.clothes_expenses += amount
                         self.total_expenses += amount
+                        category_found = True
 
-                # Round the total amount to two decimals.
-                self.total_expenses = float("{0:.2f}".format(self.total_expenses))
+                for category in self.income:
+                    if category in transaction_info:
+                        self.income_amount += amount
+                        category_found = True
+
+                for category in self.payback_loans:
+                    if category in transaction_info:
+                        self.payback_loans_amount += amount
+                        category_found = True
+
+                if not category_found:
+                    if 'överföring' not in transaction_info:
+                        self.other_expenses += amount
+                        self.total_expenses += amount
+                        self.other_list.append({transaction_info: amount})
 
             except ValueError as e:
                 print(e)
+        print(self.other_list)
+
+        # Round the values of the expenses to two decimals
+        self.total_expenses = float("{0:.2f}".format(abs(self.total_expenses)))
+        self.bills_expenses = float("{0:.2f}".format(abs(self.bills_expenses)))
+        self.pleasure_expenses = float("{0:.2f}".format(abs(self.pleasure_expenses)))
+        self.food_expenses = float("{0:.2f}".format(abs(self.food_expenses)))
+        self.other_expenses = float("{0:.2f}".format(abs(self.other_expenses)))
+        self.clothes_expenses = float("{0:.2f}".format(abs(self.clothes_expenses)))
+        self.payback_loans_amount = float("{0:.2f}".format(abs(self.payback_loans_amount)))
 
     def draw_graph(self, components):
         # Pie-chart of two separate charts.
         fig1, plot = plt.subplots()
 
         # Chart 1: Represent the different categories with how much money was spent on them.
-        labels = 'Food', 'Bills', 'Pleasure', 'Clothes', 'Others'
-        explode = (0.1, 0, 0, 0, 0)  # only "explode" the 2nd slice
+        labels = 'Food', 'Bills', 'Pleasure', 'Clothes', 'Others', 'Payback Loans'
+        explode = (0.1, 0, 0, 0, 0, 0)  # only "explode" the 2nd slice
 
         # White ring for the middle of the doughnut
         middle_circle = plt.Circle((0, 0), 0.7, color='white')
@@ -99,12 +135,13 @@ class FinancialGraphic:
         plot.text(1.5, 0, 'Food: {0} SEK'.format(self.food_expenses))
         plot.text(1.5, 0.1, 'Bills: {0} SEK'.format(self.bills_expenses))
         plot.text(1.5, 0.2, 'Pleasure: {0} SEK'.format(self.pleasure_expenses))
-        plot.text(1.5, 0.3, 'Other: {0} SEK'.format(self.other_expenses))
+        plot.text(1.5, 0.3, 'Payback Loans: {0} SEK'.format(self.payback_loans_amount))
+        plot.text(1.5, 0.4, 'Other: {0} SEK'.format(self.other_expenses))
         plot.text(1.5, -0.2, 'Total expenses: {0} SEK'.format(self.total_expenses))
 
         plt.show()
 
 
 if __name__ == '__main__':
-    path = 'C:\\Users\\Johan\\PycharmProjects\\Financial_Graphic\\excel_files\\october.xls'
+    path = 'C:\\Users\\Johan\\PycharmProjects\\Financial_Graphic\\excel_files\\december19.xls'
     FinancialGraphic(path)
