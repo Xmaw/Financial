57198fb8d7d4e77afc596b320c9b88db2f10d2c7
Xmaw <elias@Eliass-MBP.lan>
2024-09-15 21:03:44 +0200

    updated the script to both group and sort the transactions. The result is printed to the terminal

--- expenses_cli.py
+++ expenses_cli.py
@@ -12,6 +12,19 @@ def get_transactions(path):
 
     return expenses
 
+def group_transactions(transaction_list):
+    grouped_transactions = {}
+    for transaction in transaction_list:
+        info = transaction[0]
+        try:
+            amount = float(transaction[1])
+        except Exception as e:
+            continue
+        if info in grouped_transactions:
+            grouped_transactions[info] += amount
+        else:
+            grouped_transactions[info] = amount
+    return grouped_transactions
 
 
 if __name__ == '__main__':
@@ -19,9 +32,12 @@ if __name__ == '__main__':
     path = '/Users/elias/PycharmProjects/banking/2024'
     files = os.listdir(path)
     banking_files = [x for x in os.listdir(path) if ".xlsx" in x or ".csv" in x]
-
     file_index = 0
     some_file = banking_files[file_index]
     path_to_some_file = os.path.join(path, some_file)
-    print(get_transactions(path_to_some_file))
+    transactions = get_transactions(path_to_some_file)
+    grouped_transactions = group_transactions(transactions)
+    grouped_transactions = sorted(grouped_transactions.items(), key=lambda x: x[1])
+    for transaction in grouped_transactions:
+        print(transaction)
 
